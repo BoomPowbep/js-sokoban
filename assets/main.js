c2f63ef090d6a56a8911edc4e2a6f08e3b557caf
BoomPowBep <37978668+BoomPowbep@users.noreply.github.com>
2019-03-25 00:19:50 +0100

    Added credits in logs

--- assets/main.js
+++ assets/main.js
@@ -3,6 +3,9 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('           www.debalme.dev')
     console.log('    mickael.debalme@edu.gobelins.fr')
     console.log('')
+    console.log('Graphical assets: https://www.kenney.nl/assets/sokoban')
+    console.log('GitHub: https://github.com/BoomPowbep/js-sokoban')
+    console.log('')
     const game = new Game()
     document.addEventListener('gameReady', () => {
         game.run()
